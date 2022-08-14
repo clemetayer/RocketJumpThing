8b28ba827b08e803936a065bdb622486e5d3457a
clemetayer <c.lemetayer@laposte.net>
2022-08-14 11:09:44 +0200

    removed (most of) static paths for wall ride

--- Game/Common/Characters/MainCharacters/player.gd
+++ Game/Common/Characters/MainCharacters/player.gd
@@ -240,10 +240,7 @@ func _process_movement(delta):
 
 	# Wall ride wall check
 	if _RC_wall_direction == 0:  # First contact with wall
-		if $RayCasts/RayCastWallMinus.is_colliding():
-			_RC_wall_direction = -1
-		elif $RayCasts/RayCastWallPlus.is_colliding():
-			_RC_wall_direction = 1
+		_find_wall_direction()
 
 	# Movement process
 	if not is_on_floor() and _slide and _RC_wall_direction != 0 and !_wall_ride_lock:
@@ -263,6 +260,13 @@ func _process_movement(delta):
 	var snap = Vector3.ZERO if Input.is_action_pressed("movement_jump") else -get_floor_normal()
 	vel = move_and_slide_with_snap(vel, snap, Vector3.UP, true, 4, MAX_SLOPE_ANGLE, false)
 	current_speed = vel.length()
+
+
+func _find_wall_direction() -> void:
+	if get_node(PATHS.raycasts.right).is_colliding():
+		_RC_wall_direction = -1
+	elif get_node(PATHS.raycasts.left).is_colliding():
+		_RC_wall_direction = 1
 
 
 # wall ride movement management
