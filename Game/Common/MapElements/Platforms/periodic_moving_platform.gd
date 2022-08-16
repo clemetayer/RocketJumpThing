8774e3ad5bb8e8812124ad0c6d15eef9a425b361
clemetayer <c.lemetayer@laposte.net>
2022-08-16 21:53:23 +0200

    Added paths on moving platforms

--- Game/Common/MapElements/Platforms/periodic_moving_platform.gd
+++ Game/Common/MapElements/Platforms/periodic_moving_platform.gd
@@ -12,7 +12,8 @@ class_name PeriodicMovingPlatform
 
 ##### VARIABLES #####
 #---- CONSTANTS -----
-# const constant := 10 # Optionnal comment
+const PATH_TRANSPARENCY = 1.0  # alpha value of the material showing the path of the platform
+const PATH_MESH_RADIUS = 0.125  # radius of the cylinder mesh
 
 #---- EXPORTS -----
 export(Dictionary) var properties setget set_properties
@@ -30,6 +31,8 @@ var _wait_time := 0  # time to wait after reaching a point
 var _transition_type := Tween.TRANS_LINEAR  # Tween transition type
 var _ease := Tween.EASE_IN  # Tween ease
 var _desired_pos := false
+var _path_color := Color.white  # color of the platform path
+var _path_emission := 2.0  # emission intensity of the  platform path
 
 #==== ONREADY ====
 onready var onready_translation := self.translation  # keeps a record of the original position of the platform
@@ -62,6 +65,12 @@ func _ready():
 		_transition_type = properties["trans_type"]
 	if properties.has("ease"):
 		_ease = properties["ease"]
+	if properties.has("path_color"):
+		_path_color = properties["path_color"]
+	if properties.has("path_emission"):
+		_path_emission = properties["path_emission"]
+	# sets the path meshes
+	_create_show_path_meshes()
 	# sets the first step
 	if _pos_array.size() > 1:
 		_set_tween_to_pos_idx(_step_idx)
@@ -89,6 +98,48 @@ func update_properties() -> void:
 
 
 #==== Other =====
+# adds (all) the path meshes to the parent
+func _create_show_path_meshes() -> void:
+	for pos_idx in range(1, _pos_array.size()):
+		var mesh = _create_show_path_mesh(_pos_array[pos_idx - 1], _pos_array[pos_idx])
+		var mid_pos = (_pos_array[pos_idx - 1] - _pos_array[pos_idx]) / 2.0  # middle position, since a cylinder mesh starts at the middle
+		mesh.transform.origin += self.transform.origin - mid_pos + _pos_array[pos_idx - 1]  # adds the position of the platform in the world
+		get_parent().call_deferred("add_child", mesh)
+
+
+# adds (one) path mesh to the parent
+func _create_show_path_mesh(start_point: Vector3, end_point: Vector3) -> MeshInstance:
+	var mesh_instance := MeshInstance.new()
+	mesh_instance.mesh = _create_cylinder_mesh(start_point, end_point)
+	var normalized_vect := (end_point - start_point).normalized()  # used to check if the vector is aligned with the UP vector
+	if normalized_vect == Vector3.UP or normalized_vect == -Vector3.UP:  # aligned with up vector, do not use look_at
+		mesh_instance.rotation.x = PI / 2.0
+	else:
+		mesh_instance.look_at(end_point - start_point, Vector3.UP)
+	mesh_instance.set_surface_material(0, _create_path_material())
+	return mesh_instance
+
+
+# creates a cylinder mesh matching the path
+func _create_cylinder_mesh(start_point: Vector3, end_point: Vector3) -> CapsuleMesh:
+	var mesh := CapsuleMesh.new()
+	mesh.radius = PATH_MESH_RADIUS
+	mesh.mid_height = abs(start_point.distance_to(end_point))
+	return mesh
+
+
+# creates the material for the path
+func _create_path_material() -> SpatialMaterial:
+	var material := SpatialMaterial.new()
+	material.flags_transparent = true
+	_path_color.a = PATH_TRANSPARENCY
+	material.albedo_color = _path_color
+	material.emission_enabled = true
+	material.emission = _path_color
+	material.emission_energy = _path_emission
+	return material
+
+
 func _set_tween_to_pos_idx(idx: int) -> void:
 	_tween.interpolate_property(
 		self,
