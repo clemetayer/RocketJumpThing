86931f4cb13130bf83489a41409d96dcb8502177
clemetayer <c.lemetayer@laposte.net>
2022-09-12 20:56:46 +0200

    added 'tests' for camera_utils

---
+++ test/UnitTests/Camera/camera_utils_test.gd
@@ -0,0 +1,19 @@
+# GdUnit generated TestSuite
+#warning-ignore-all:unused_argument
+#warning-ignore-all:return_value_discarded
+extends GlobalTests
+# tests some camera utils
+
+##### VARIABLES #####
+const camera_utils_path := "res://test/UnitTests/Camera/camera_utils_mock.tscn"
+
+
+##### TESTS #####
+#---- PRE/POST -----
+func before():
+	element_path = camera_utils_path
+	.before()
+
+#---- TESTS -----
+#==== ACTUAL TESTS =====
+# TODO : hard to test the camera_utils because it is mainly tweens
