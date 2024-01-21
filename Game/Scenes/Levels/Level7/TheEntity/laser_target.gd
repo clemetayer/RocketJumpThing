0abdb8d0e2be242b9edb78052129479e0a107b9a
clemetayer <c.lemetayer@laposte.net>
2024-01-21 16:00:58 +0100

    Fixed an issue on the laser target of the entity, sometimes displaying at a weird angle from the floor

--- Game/Scenes/Levels/Level7/TheEntity/laser_target.gd
+++ Game/Scenes/Levels/Level7/TheEntity/laser_target.gd
@@ -1,16 +1,13 @@
 extends Sprite3D
 # pretty much the same of the rocket target
-# Refactor : self explanatory, just need to find the correct folder and name and we're good
 
 
 ##### PUBLIC METHODS #####
 func set_pos(hit_pos: Vector3, normal: Vector3) -> void:
 	global_transform.origin = hit_pos
 	if (
-		normal == Vector3.ZERO
-		or normal.normalized() == Vector3.UP
-		or normal.normalized() == Vector3.DOWN
+		normal != Vector3.ZERO
+		and normal.normalized() != Vector3.UP
+		and normal.normalized() != Vector3.DOWN
 	):
-		rotation.x = PI / 2.0
-	else:
-		look_at(hit_pos + normal.normalized(), Vector3.UP)
+		look_at(hit_pos + normal, Vector3.UP)
