d73d948b4cea73677bf447ec68afaa5794befe9f
clemetayer <c.lemetayer@laposte.net>
2024-01-31 21:33:01 +0100

    Disabled debugDraw

--- Game/Common/Utils/Static/global_parameters.gd
+++ Game/Common/Utils/Static/global_parameters.gd
@@ -6,6 +6,6 @@ class_name GlobalParameters
 #---- DEBUG -----
 const TUTORIALS_ENABLED := true  # to avoid triggering tutorials when testing levels (it gets annoying, I know how to play dammit, I created it)
 const DEBUG_DRAW_ENABLED := {
-	"set_text": true, "draw_line_3d": false, "draw_ray_3d": false, "draw_box": false
+	"set_text": false, "draw_line_3d": false, "draw_ray_3d": false, "draw_box": false
 }
 const DEATHGRID_ENABLED := true  # especially usefull in the map building phase
